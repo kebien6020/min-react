b560351a4521a496ab82ae5fec59537bcad149ac
Kevin Peña <kevin.pena.prog@gmail.com>
2020-12-23 10:05:43 -0500

    Initial commit

---
+++ webpack.config.ts
@@ -0,0 +1,30 @@
+import * as webpack from 'webpack'
+import * as path from 'path'
+import HtmlWebpackPlugin from 'html-webpack-plugin'
+import { CleanWebpackPlugin }  from 'clean-webpack-plugin'
+
+const config : webpack.Configuration = {
+  mode: 'development',
+  entry: './src/index.ts',
+  output: {
+    filename: '[name].js',
+    path: path.resolve(__dirname, 'public'),
+  },
+  module: {
+    rules: [
+      {
+        test: /\.tsx?$/,
+        loader: 'babel-loader',
+      },
+    ],
+  },
+  plugins: [
+    new HtmlWebpackPlugin(),
+    new CleanWebpackPlugin(),
+  ],
+  resolve: {
+    extensions: ['.ts', '.tsx', '.js'],
+  },
+}
+
+export default config
